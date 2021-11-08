2a6ab835811c83de9cddcdff79679e6dd1d24192
İhlas Mert <ihlas1mert@gmail.com>
2021-11-08 08:44:44 +0300

    Initial Commit

---
+++ player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  player
+//
+//  Created by ihlas on 8.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
